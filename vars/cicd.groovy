7550825ee0bcfe97300d263a65c0f42e94b692b7
agastyadevops <118795694+agastyadevops@users.noreply.github.com>
2023-01-30 20:22:34 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,3 +12,6 @@ def newDeploy(workspace,ip,context)
    sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }  
   
+def newselenium(workspace)
+sh 'java -jar /var/lib/jenkins/workspace/${workspace}/testing.jar'
+
